1849d204ba55f233faa03c6d922832aa565a8a28
kevin892 <kevinbruggiero@gmail.com>
2020-08-30 10:27:37 -0600

    clean

--- main.go
+++ main.go
@@ -11,28 +11,46 @@ import (
 
 var serverRequests int
 var averageTime float64
-var allTimes float64
+var requestTimes float64
+var finishedPasswords []string
+
+func hashPassword(password string) {
+	hashedPassword := sha512.Sum512([]byte(password))
+	encodePassword(hashedPassword)
+}
+
+func encodePassword(password [64]byte) {
+	encodedPassword := base64.StdEncoding.EncodeToString(password[:])
+	finishedPasswords = append(finishedPasswords, encodedPassword)
+}
+
+func addToCounter(duration int64) {
+	requestTimes = requestTimes + float64(duration)
+	serverRequests++
+}
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.String() {
 	case "/hash":
 		startTime := time.Now()
 		r.ParseForm()
-		password := r.Form.Get("password")
-		hashedPassword := sha512.Sum512([]byte(password))
-		encodedPassword := base64.StdEncoding.EncodeToString(hashedPassword[:])
-		duration := time.Since(startTime).Microseconds()
-		time.Sleep(5 * time.Second)
-		fmt.Fprintf(w, encodedPassword)
-		allTimes = allTimes + float64(duration)
-		serverRequests++
+		if r.Form.Get("password") == "" {
+			fmt.Fprintf(w, "Error - 'password' not found!")
+		} else {
+			password := r.Form.Get("password")
+			hashPassword(password)
+			duration := time.Since(startTime).Microseconds()
+			time.Sleep(5 * time.Second)
+			fmt.Fprintf(w, finishedPasswords[serverRequests])
+			addToCounter(duration)
+		}
 	case "/shutdown":
 		log.Fatalf("%s\n", "Shutting down")
 	case "/stats":
 		if serverRequests == 0 {
 			fmt.Fprintf(w, "average: %v\n", 0)
 		} else {
-			fmt.Fprintf(w, "average: %v\n", allTimes/float64(serverRequests))
+			fmt.Fprintf(w, "average: %v\n", requestTimes/float64(serverRequests))
 		}
 		fmt.Fprintf(w, "total: %v\n", serverRequests)
 	default:
